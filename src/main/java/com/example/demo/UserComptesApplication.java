a0763a10b815e737596ec9ae8efd52912a0c6fc6
Yassin TALSSIS <47031764+YassinTalssis@users.noreply.github.com>
2023-01-13 18:17:26 +0100

    Update UserComptesApplication.java

--- src/main/java/com/example/demo/UserComptesApplication.java
+++ src/main/java/com/example/demo/UserComptesApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UserComptesApplication {
-
+	//YassinTalssis
 	public static void main(String[] args) {
 		SpringApplication.run(UserComptesApplication.class, args);
 	}
